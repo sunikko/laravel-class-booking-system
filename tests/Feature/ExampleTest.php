69174bebe2cf1cf8054973e863c082838573e3bf
Soy <sunikko77@gmail.com>
2026-03-28 16:17:14 +0000

    feat: Commit default example feature test file; Enhance root route test with authenticated user scenario

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\User;
+
 it('returns a successful response', function () {
     $response = $this->get('/');
-
     $response->assertStatus(200);
+
+    $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/');
+
+    $response->assertStatus(302);
+    $response->assertRedirect('/bookings');
 });
